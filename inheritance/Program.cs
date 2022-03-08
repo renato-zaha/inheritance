9f31701bad7eccb06a86dae0d8481e139f017b3b
Renato Zaha <renato.zaha@gmail.com>
2022-03-08 11:48:05 -0300

    Exercicio inheritance

---
+++ inheritance/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using inheritance.Entities;
+using System.Globalization;
+
+List<Employee> list = new List<Employee>();
+
+Console.Write("Enter the number of employees: ");
+int n =int.Parse(Console.ReadLine());
+
+for(int i=1;i<=n; i++)
+{
+    Console.Write($"Employee #{i} data:");
+    Console.Write("Oursourced (y/n)? ");
+    char ch = char.Parse(Console.ReadLine());
+    Console.Write("Name: ");
+    string name = Console.ReadLine();
+    Console.Write( "Hours: ");
+    int hours = int.Parse(Console.ReadLine());
+    Console.Write("Value per hour: ");
+    double valuePerHour  = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);    
+    if (ch == 'y')
+    {
+        Console.Write("Additional charge: ");
+        double additionalCharge = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+        list.Add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));  
+    }
+    else
+    {
+        list.Add(new Employee(name, hours, valuePerHour));
+    }
+}
+Console.WriteLine();
+Console.WriteLine("PAYMENTS:");
+foreach(Employee employee in list)
+{
+    Console.WriteLine(employee.Name + " - $" + employee.Payment().ToString("F2"),CultureInfo.InvariantCulture);
+}
